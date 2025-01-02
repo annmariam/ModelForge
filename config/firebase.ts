f32f58f9856e3224355e1339c11075fed77763b7
Rab Lap-Win10 <abrahul02@gmail.com>
2025-01-02 23:59:20 +0530

    Rename firebase configuration file to TypeScript, update initialization logic to prevent multiple app instances

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,11 +1,7 @@
-
-import "firebase/firestore";
-
 import { getAuth } from "firebase/auth";
-import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { initializeApp, getApp, getApps } from "firebase/app";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -15,8 +11,7 @@ const firebaseConfig = {
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
 
